a0ba80fbf959d92b01b5262dae3e1759fa6c1941
Presley <presleyanne@gmail.com>
2014-10-17 17:08:08 -0400

    Fix task.js to assign var after load.

--- javascript/task.js
+++ javascript/task.js
@@ -1,22 +1,16 @@
-/* User Steps
- 1. Put the JSON file describing your experiment in psiturk/static/js
- 2. Set mySperiment to the name of your experiment from the JSON file
- 3. Add speriment.js to psiturk/static/lib.
- 4. Add 
-		<script src="static/lib/speriment.js" type="text/javascript"> </script>
-	to the list of static/lib/ scripts in psiturk/templates/exp.html.
-*/
-
-var mySperiment = latin_square_experiment;
+/* Remember to import speriment.js and your JSON file in exp.html.
+ * Change the variable assigned to jsonExperiment below to your experiment's variable name.
+ */
 
 /*******************
  * Run Task
  ******************/
 $(document).ready(function(){
+	var jsonExperiment = my_exp;
 	var psiturk = PsiTurk(uniqueId, adServerLoc);
 	psiturk.finishInstructions();
-	var speriment = new Survey(mySperiment, condition, psiturk);
-	speriment.start();
+	var experiment = new Experiment(jsonExperiment, condition, psiturk);
+	experiment.start();
 });
 
 // vi: noexpandtab tabstop=4 shiftwidth=4
